e2939c176d369b7987dde4283bab62b448cdcd72
dylanengr290 <mr.dturi@gmail.com>
2026-07-09 13:43:26 -0400

    Add androidx.annotation dependency

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -36,6 +36,7 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.constraintlayout)
     implementation(libs.material)
+    implementation(libs.annotation)
     testImplementation(libs.junit)
     androidTestImplementation(libs.espresso.core)
     androidTestImplementation(libs.ext.junit)
