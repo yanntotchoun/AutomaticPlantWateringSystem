38a49d3a98a9e3db1d6f78e4e224f84b77f2e883
WillMoor <gabe.wilardi@gmail.com>
2026-07-25 00:20:39 -0400

    Update mainPlant.ino
    Added water flow sensor logic and database logging values from it

--- firmware/mainPlant.ino
+++ firmware/mainPlant.ino
@@ -59,6 +59,7 @@ int pumping_time = 3000;
 
 // Variables to track water flow through the pump
 double flow_rate = 0.0;
+double total_flow = 0.0;
 volatile int pulse_count = 0;
 const double pulses_per_liter = 5880.0;
 
@@ -67,6 +68,8 @@ void IRAM_ATTR flow_rate_ISR() {
   pulse_count++;
 }
 
+// State variable for the status of database upload requests
+volatile bool upload_complete = true;
 
 void ConnectedToAP_Handler(WiFiEvent_t wifi_event, WiFiEventInfo_t wifi_info) {
   Serial.println("Connected To The WiFi Network");
@@ -204,31 +207,55 @@ void loop() {
     }
   }
   
+  // Runs in the condition that the water pump is set to turn ONLY if set in the current loop iteration
   if (current_pump_state == 1 && previous_pump_state == 0) {
+
     // Turn the water pump on for a set duration
     digitalWrite(water_pump, HIGH);
     delay(pumping_time);
     digitalWrite(water_pump, LOW);
-    flow_rate = (pulse_count / pulses_per_liter); // Gives the amount of water dispensed in L
+    total_flow = (pulse_count / pulses_per_liter); // Gives the amount of water dispensed in L
 
     Serial.print("Water dispensed: ");
-    Serial.print(flow_rate);
+    Serial.print(total_flow);
     Serial.println(" L");
 
-    flow_rate = flow_rate * 20; // Converts the dispensed water amount from L to L/sec
+    flow_rate = total_flow * 20; // Converts the dispensed water amount from L to L/sec
 
     Serial.print("Flow rate: ");
     Serial.print(flow_rate);
     Serial.println(" L/min");
+
+    pulse_count = 0; // Reset the pulse counter of the flow sensor
+    unsigned long start = millis();
+
+    // Make sure to wait until database is free to handle requests, as the next two requests are critical and cannot be skipped until next loop iteration
+    upload_complete = false;
+    while(!app.ready()){} 
+  
+    Database.set<double>(aClient, "/plant1/dispensed_water", total_flow, processData, "RTDB_Send_DispensedWater_Volume");
+    while (!upload_complete && millis() - start < 20000){
+      app.loop();
+      delay(1);
+    }
+
+    start = millis();
+    upload_complete = false;
+    while(!app.ready()){} 
+
+    Database.set<double>(aClient, "/plant1/dispensed_flow_rate", flow_rate, processData, "RTDB_Send_Dispensed_Flow_Rate");
+    while (!upload_complete && millis() - start < 20000){
+      app.loop();
+      delay(1);
+    }
+
   }
 
   // Update the previous water pump state for next loop iteration
   previous_pump_state = current_pump_state; 
+  pulse_count = 0;
 
-  current_pump_state = 0; // FOR TESTING
-
-  
-  delay(20000); // Delay for two seconds before the next reading
+  delay(2000); // Delay for two seconds before the next reading
 }
 
 void processData(AsyncResult &result) {
@@ -237,11 +264,20 @@ void processData(AsyncResult &result) {
 
   if (result.isError()) {
     Firebase.printf("Upload failed: %s\n", result.error().message().c_str());
+    if (result.uid() == "RTDB_Send_DispensedWater_Volume" || result.uid() == "RTDB_Send_Dispensed_Flow_Rate") {
+      upload_complete = true;
+    }
     return;
   }  
 
-  if (result.available())
+  if (result.available()) {
     Firebase.printf("Upload successful: %s\n", result.uid().c_str());
+    // Water flow and volume data case interrupt CPU and must be have their condition variable flipped when completed
+    if (result.uid() == "RTDB_Send_DispensedWater_Volume" || result.uid() == "RTDB_Send_Dispensed_Flow_Rate") {
+      upload_complete = true;
+    }
+  }
     
 }
-}
+
+
